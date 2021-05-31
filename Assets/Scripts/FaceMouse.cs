728755d832f7d6c2deb6e0379ed249a8bc71aa3a
Stephen Hubbard <hubbardgames@gmail.com>
2021-05-31 10:31:24 -0600

    tilemap mining

---
+++ Assets/Scripts/FaceMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceMouse : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        faceMouse();
+    }
+
+    private void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(transform.position.x - mousePosition.x, transform.position.y - mousePosition.y);
+
+        transform.up = direction;
+    }
+}
--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerActions : MonoBehaviour
 {
+    [SerializeField] private Tilemap foregroundTilemap;
+    [SerializeField] private Transform hitSpot;
+    
     private PlayerControls playerControls;
+    
 
     private void Awake() {
         playerControls = new PlayerControls();
@@ -28,7 +33,20 @@ public class PlayerActions : MonoBehaviour
         
     }
 
+    private bool isTile() {
+        Vector3Int gridPosition = foregroundTilemap.WorldToCell((Vector3)hitSpot.position);
+        if (foregroundTilemap.HasTile(gridPosition)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     private void useAction() {
-        
+        if (isTile()) {
+            Vector3Int gridPosition = foregroundTilemap.WorldToCell((Vector3)hitSpot.position);
+            foregroundTilemap.SetTile(gridPosition, null);
+        }
     }
 }
